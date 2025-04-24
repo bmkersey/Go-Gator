70eed1deaf5e0a583721885cb18e5fda69d4dd2e
Brendan <bmkersey@gmail.com>
2025-04-24 00:03:07 -0400

    added ability to leave agg running in the background and use program in a dif terminal

--- handlerAgg.go
+++ handlerAgg.go
@@ -3,15 +3,45 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 )
 
 func handlerAgg(s *state, cmd command) error {
-	feed, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
-	if err != nil {
-		return err
+	if len(cmd.args) < 1 {
+		return fmt.Errorf("command agg takes 1 argument: time_between_reqs")
 	}
 
-	fmt.Printf("%+v\n", feed)
+	timeBetweenReqs, err := time.ParseDuration(cmd.args[0])
+	if err != nil {
+		return fmt.Errorf("error parsing duration: %s", err)
+	}
+
+	fmt.Printf("Collecting feeds every %v\n", timeBetweenReqs)
+	ticker := time.NewTicker(timeBetweenReqs)
+	for ; ; <-ticker.C {
+		scrapeFeeds(s)
+	}
+}
+
+func scrapeFeeds(s *state) error {
+	nextFeed, err := s.db.GetNextFeedToFetch(context.Background())
+	if err != nil {
+		return fmt.Errorf("error getting next feed: %s", err)
+	}
+
+	err = s.db.MarkFeedFetched(context.Background(), nextFeed.ID)
+	if err != nil {
+		return fmt.Errorf("failed to mark feed as fetched: %s", err)
+	}
+
+	fetchedFeed, err := fetchFeed(context.Background(), nextFeed.Url)
+	if err != nil {
+		return fmt.Errorf("error fetching RSS feed: %s", err)
+	}
+
+	for _, f := range fetchedFeed.Channel.Item {
+		fmt.Println(f.Title)
+	}
 
 	return nil
 }
--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -19,3 +19,15 @@ ON feeds.user_id = users.id;
 -- name: GetFeed :one
 SELECT * FROM feeds
 WHERE feeds.url = $1;
+
+-- name: MarkFeedFetched :exec
+UPDATE feeds
+SET 
+  last_fetched_at = CURRENT_TIMESTAMP,
+  updated_at = CURRENT_TIMESTAMP
+WHERE id = $1;
+
+-- name: GetNextFeedToFetch :one
+SELECT * FROM feeds
+ORDER BY last_fetched_at ASC NULLS FIRST
+LIMIT 1;
